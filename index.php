0ca1593cf8a1c11b81d345d5505146e21ebbd004
xhdmmxs <pnopolo@gmail.com>
2016-09-09 00:14:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-<title>Home</title>
+<title>Book Cloud</title>
 </head>
-<body>
-
+	<body style="margin:0px;padding:0px;overflow:hidden">
+	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/forty/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
 
 <?php include('asite.php');?>
 </body>
