53ebb32ac191fa4204fc7b26b6029663398e044d
xhdmmxs <pnopolo@gmail.com>
2016-09-09 00:16:14 +0700

    Update asite.php

--- asite.php
+++ asite.php
@@ -7,7 +7,7 @@ echo '<div style="display:none">';
 foreach($all as $file){
 $file= str_replace(array('keyw/', '.txt'), '', $file);
 $file= str_replace(' ', '-_-', $file);
- echo '<a href="/force/'.$file.'" title="file '.$file.'">'.$file.'</a> | ';
+ echo '<a href="/page/'.$file.'" title="file '.$file.'">'.$file.'</a> | ';
 }
 
 echo '</div>';
