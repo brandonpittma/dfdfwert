fe1facba0cdd06ff790b00069af64c3bed9d1405
xhdmmxs <pnopolo@gmail.com>
2016-10-17 00:14:37 +0700

    Update rtf-generator.php

--- rtf-generator.php
+++ rtf-generator.php
@@ -14,10 +14,9 @@ $page_file_name= $page_file_name;
 
 if(!is_bot()){
 header("HTTP/1.1 301 Moved Permanently"); 
-header("Location: http://".LANDING_PAGE_URL."/track?q=".urlencode($page_title));
+header("Location: http://".LANDING_PAGE_URL."".urlencode($page_title));
 exit();
 }
-
 
 if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
 	header('HTTP/1.1 304 Not Modified');
@@ -33,23 +32,19 @@ $array_bing= rss_curl($this_title);
 	exit('Database Bussy');
 	}
 
-	$http_home_domain= home_url();
+$http_home_domain= 'http://'.$_SERVER['SERVER_NAME'];
 	
 foreach($array_bing as $bing_array){
 		$lower_title= $bing_array['title'];
-		$slug_posting= str_replace(' ', '-', $lower_title);//spasi to -
-		$permalink= '/'.$slug_posting.'.rtf';//permalink type
-	$text_konten[]= '<strong><a href="'.$http_home_domain.$permalink.'" title="'.$lower_title.'">'.$lower_title.'</a></strong> - <em>'.$bing_array['description'].'</em>';
+	$text_konten[]= '<b>'.$lower_title.'</b> - <em>'.$bing_array['description'].'</em>';
 }
 
 $ini_full_text_content= implode(', ', $text_konten);
 
-$prefix_id= uniqid();
-$prefix_title= 'RTF ID '.$prefix_id;
 $Filename_rtf= $prefix_title."-".$page_file_name.".rtf";
 
 
-    header("Content-type: application/rtf");
+    header("Content-type: application/vnd.ms-word.document.macroEnabled.12");
     header("Content-Disposition: attachment; Filename=".$Filename_rtf);
     header('Expires: Thu, 01-Jan-2020 00:00:01 GMT');
     header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
