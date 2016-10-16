24001c04e3906ffdc5a034e0e7cb925ae3b63eb4
xhdmmxs <pnopolo@gmail.com>
2016-10-17 00:20:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="en" data-form-id="">
+  <head>
 <head>
-<title>Book Cloud</title>
-</head>
-	<body style="margin:0px;padding:0px;overflow:hidden">
-	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/forty/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
+	<title>DemoSite - ReadOnly</title>
+	</head>
 
-<?php include('asite.php');?>
-</body>
+	<body style="margin:0px;padding:0px;overflow:hidden">
+	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/read-only/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
+	<?php include('asite.php');?>
+	<?php include('asite-pdf.php');?>
+        <?php include('asite-doc.php');?>
+  	<?php include('asite-rtf.php');?>
+	</body>
+
 </html>
+
