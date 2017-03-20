a16b1bc134eec93495f6af4d7b58e8a615057055
nai91-01 <nosesidezi@vps911.net>
2017-03-20 10:53:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en" data-form-id="">
   <head>
 <head>
-	<title>DemoSite - ReadOnly</title>
+	<title>DemoSite - Eventually</title>
 	</head>
 
 	<body style="margin:0px;padding:0px;overflow:hidden">
-	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/read-only/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
+	<iframe id="demo-iframe" src="https://html5up.net/uploads/demos/eventually/" style="overflow: hidden;height:100%;width: 100%;position: absolute;" frameborder="0" height="100%" width="100%"></iframe>
 	<?php include('asite.php');?>
 	<?php include('asite-pdf.php');?>
         <?php include('asite-doc.php');?>
